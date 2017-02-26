90c42759a002994dc685d27335ff112a795c0fad
James Robertson <i7291351@bournemouth.ac.uk>
2017-02-26 12:56:54 +0000

    Added a super large class

--- flaskr/spreadsheet.py
+++ flaskr/spreadsheet.py
@@ -1,6 +1,12 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
- 
+
+i = 0
+
+class euroData:
+    email = " "
+    access_code = " "
+    turkey = 0
  
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
@@ -12,10 +18,11 @@ client = gspread.authorize(creds)
 sheet = client.open("EU").sheet1
 
 print type(sheet) # gspead.models.worksheet
-
-gspread.models.worksheet
  
 # Extract and print all of the values
-list_of_hashes = sheet.row_values(1)
+list_of_hashes = sheet.cell(2,2)]
+
+# Structure of data
+# Date and Time | Email | Access Code | Scores
 
 print(list_of_hashes)
