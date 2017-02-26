c530f8d7a88b3cbd1cc0407eff6ff8089fbba1b8
James Robertson <i7291351@bournemouth.ac.uk>
2017-02-26 14:05:59 +0000

    It works! I can read in data into an object!

--- flaskr/spreadsheet.py
+++ flaskr/spreadsheet.py
@@ -2,11 +2,16 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 i = 0
+j = 0
 
-class euroData:
+class euroData(object):
     email = " "
     access_code = " "
     turkey = 0
+
+sampleData = euroData()
+
+allData = []
  
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
@@ -20,9 +25,22 @@ sheet = client.open("EU").sheet1
 print type(sheet) # gspead.models.worksheet
  
 # Extract and print all of the values
-list_of_hashes = sheet.cell(2,2)]
+list_of_hashes = sheet.cell(2,2)
+
+while i < sheet.col_count:
+    while j < sheet.row_count:
+        if i == 2:
+            sampleData.email = sheet.cell(i,j)
+        if i == 3:
+            sampleData.access_code = sheet.cell(i,j)
+        if i == 4:
+            sampleData.turkey = sheet.cell(i,j)
+        i += 1
+        j += 1
+    allData.append(sampleData)
+    
 
 # Structure of data
 # Date and Time | Email | Access Code | Scores
 
-print(list_of_hashes)
+print(allData)
